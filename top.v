a8e8ac2d31afc0d9a819a661be7bca0c218a0a23
Roenan Bingle <134002753+Roenan-b@users.noreply.github.com>
2025-09-08 13:15:35 -0700

    Update top.v
    Took out unnecessary stuff

--- top.v
+++ top.v
@@ -10,11 +10,9 @@ output CA, CB, CC, CD, CE, CF, CG; //segment a, b, ... g
 output [7:0] AN; //enable each digit of the 8 digits
 //write your code to connect the modules as shown in Figure 1 (page 3)
 wire ClkOut;
-ClkDiv a1(CLK100MHZ, 1'b0, ClkOut);
+  ClkDiv a1(Clk, 1'b0, ClkOut);
 wire [7:0] R_Data, max;
-lab6_partA a2(ClkOut, BTNU, 1'b1, LED, max, R_Data);
 wire [7:0] D;
-Lab6_2x1Mux a3(max, R_Data, LED, D);
 Two4DigitDisplay a4(CLK100MHZ, D[6:0], CA,CB,CC,CD,CE,CF,CG,AN);
 
 
