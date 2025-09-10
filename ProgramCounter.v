9ed696deac8d73e29fc83333aa316a9c097e4f63
Roenan Bingle <134002753+Roenan-b@users.noreply.github.com>
2025-09-10 13:01:07 -0700

    Update ProgramCounter.v

--- ProgramCounter.v
+++ ProgramCounter.v
@@ -16,7 +16,7 @@
 //
 // FUNCTIONALITY:-
 // Design a program counter register that holds the current address of the 
-// instruction memory.  This module should be updated at the positive edge of 
+// instruction memory. This module should be updated at the positive edge of 
 // the clock. The contents of a register default to unknown values or 'X' upon 
 // instantiation in your module.  
 // You need to enable global reset of your datapath to point 
@@ -26,17 +26,17 @@
 
 module ProgramCounter(Address, PCResult, Reset, Clk);
 
-	input [31:0] Address;	//Given
-	input Reset, Clk;	//Given
-	//input [31:0] PCAddResult;
-	output reg [31:0] PCResult;	//Given
+	input [31:0] Address;	    // 32-bit input carrying the "next PC" value (e.g., PC + 4)
+	input Reset, Clk;		    // Reset = async control to clear PC to 0; Clk = clock signal
+	//input [31:0] PCAddResult;	// Unused input (likely for PC + 4 in other designs)
+	output reg [31:0] PCResult;	// 32-bit register output that stores the current PC value
 	
+ // Always block triggered on the rising edge of the clock
  always @ (posedge Clk) begin
     if (Reset == 1) begin
-       PCResult <= 0;
+       PCResult <= 0;           // If Reset is asserted, set PC to 0 (start of instruction memory)
     end
 	 else 
-		 PCResult <= Address;         // next PC value (e.g., PC+4) think this needs to be switched to Address, Address is holding the next value
+		 PCResult <= Address;   // Otherwise, update PCResult with the incoming Address (next PC)
  end
 endmodule
-
