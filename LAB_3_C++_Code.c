35027fb12829c32037f0d5b954921a5d2ec0be15
nmonroe04 <167247190+nmonroe04@users.noreply.github.com>
2025-09-18 14:37:50 -0700

    Update LAB_3_C++_Code.c

--- LAB_3_C++_Code.c
+++ LAB_3_C++_Code.c
@@ -1,1 +1,21 @@
 
+int frame_x;
+int fame_y;
+int window_x;
+int window_y;
+
+
+int sad_x = frame_x - window_x;
+int sad_y = frame_y - window_y;
+
+// movement 1 (x cord + 1)
+
+
+
+
+//movement 2 (y cord - 1)
+
+
+//movement 3 (diagonal up-right)
+
+//movement 4 (diagonal left-down)
