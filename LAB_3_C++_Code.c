26a68879b6482f2df5a130f936856f919893fdc4
nmonroe04 <167247190+nmonroe04@users.noreply.github.com>
2025-09-18 14:53:37 -0700

    Update LAB_3_C++_Code.c

--- LAB_3_C++_Code.c
+++ LAB_3_C++_Code.c
@@ -7,6 +7,10 @@ int window_y;
 
 int sad_x = frame_x - window_x;
 int sad_y = frame_y - window_y;
+int sad_Min = 1000000;
+
+
+
 
 // movement 1 (x cord + 1)
 
@@ -19,3 +23,22 @@ int sad_y = frame_y - window_y;
 //movement 3 (diagonal up-right)
 
 //movement 4 (diagonal left-down)
+
+
+// sad capture
+
+function sad_capture (int window_x_size, int window_y_size, int frame_i, int frame,j){
+  int SAD =0;
+  for (int i=1; i < window_x_size; i++){
+  for (int j =1; i < window_y_size; j++){
+  SAD = SAD + abs(array[i][j] - window[i][j]);  //IN MIPS IT WOULD BE array[i*j] or window[i*j]
+  }
+  }
+  if (SAD < sad_min){
+  sad_min = SAD;   //CHECK THIS, either implement the comparison in the function or outside of it.
+  }
+  return SAD;
+}
+
+
+
