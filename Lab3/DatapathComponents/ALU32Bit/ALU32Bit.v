8d3c2d51c086cf608f337e2fa9e564ca357306bc
eharris2 <166444973+eharris2@users.noreply.github.com>
2025-10-06 14:12:47 -0700

    Update ALU32Bit.v

--- Lab3/DatapathComponents/ALU32Bit/ALU32Bit.v
+++ Lab3/DatapathComponents/ALU32Bit/ALU32Bit.v
@@ -32,56 +32,10 @@ module ALU32Bit(ALUControl, A, B, ALUResult, Zero);
                                 // you need to adjust the bitwidth as needed
 	input [31:0] A, B;	    // inputs
 
-	output reg [31:0] ALUResult;	// answer
+	output [31:0] ALUResult;	// answer
 	output Zero;	    // Zero=1 if ALUResult == 0
 
-
-// ---- Operation map---- Need to edit the mappings
-    localparam ALU_AND  = 4'b0000;
-    localparam ALU_OR   = //4'b0001;
-    localparam ALU_ADD  = //4'b0010;
-    localparam ALU_XOR  = //4'b0011;
-    localparam ALU_SLL  = //4'b0100; // B << A[4:0]
-    localparam ALU_SRL  = //4'b0101; // B >> A[4:0] (logical)
-    localparam ALU_SUB  = //4'b0110;
-    localparam ALU_SLT  = //4'b0111; // signed
-    localparam ALU_NOR  = //4'b1100;
-    localparam ALU_SRA  = //4'b1001; // B >>> A[4:0] (arithmetic)
-    localparam ALU_SLTU = //4'b1010; // unsigned
-    localparam ALU_LUI  = //4'b1011; // {B[15:0], 16'b0}
-    localparam ALU_PASSA= //4'b1110; // result = A
-    localparam ALU_PASSB= //4'b1111; // result = B
-
-    wire [4:0] shamt = A[4:0];
-
-    always @* begin
-        case (ALUControl)
-            ALU_AND  : ALUResult = A & B;
-            ALU_OR   : ALUResult = A | B;
-            ALU_XOR  : ALUResult = A ^ B;
-            ALU_NOR  : ALUResult = ~(A | B);
-
-            ALU_ADD  : ALUResult = A + B;
-            ALU_SUB  : ALUResult = A - B;
-
-            ALU_SLT  : ALUResult = ($signed(A) < $signed(B)) ? 32'd1 : 32'd0;
-            ALU_SLTU : ALUResult = (A < B)                   ? 32'd1 : 32'd0;
-
-            ALU_SLL  : ALUResult = (B <<  shamt);
-            ALU_SRL  : ALUResult = (B >>  shamt);
-            ALU_SRA  : ALUResult = ($signed(B) >>> shamt);
-
-            ALU_LUI  : ALUResult = {B[15:0], 16'b0};
-
-            ALU_PASSA: ALUResult = A;
-            ALU_PASSB: ALUResult = B;
-
-            default  : ALUResult = 0; // defensive default
-        endcase
-    end
-
-    assign Zero = (ALUResult == 32'b0);
+    /* Please fill in the implementation here... */
 
 endmodule
-
 
