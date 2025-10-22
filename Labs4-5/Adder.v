2081838f090d7c45696add4fe373d041d00d7ee0
eharris2 <166444973+eharris2@users.noreply.github.com>
2025-10-22 14:36:37 -0700

    Update Adder.v

--- Labs4-5/Adder.v
+++ Labs4-5/Adder.v
@@ -6,7 +6,7 @@ module Adder(PCAddResult, immSL2, InstructionSig);
 
     // Always block triggers whenever PCResult changes
   always @ (*) begin
-        InstructionSig <= PCAddResult + innSL2; // Increment the PC by 4 (word-aligned instruction step)
+        InstructionSig <= PCAddResult + immSL2; // Increment the PC by 4 (word-aligned instruction step)
     end
   
 endmodule
