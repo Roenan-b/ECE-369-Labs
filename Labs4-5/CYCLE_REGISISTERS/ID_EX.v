9bc5926a882fcf8a06d61c9e3ea04a93242eb5ba
eharris2 <166444973+eharris2@users.noreply.github.com>
2025-10-27 14:16:51 -0700

    Create ID_EX.v

---
+++ Labs4-5/CYCLE_REGISISTERS/ID_EX.v
@@ -0,0 +1,5 @@
+`timescale 1ns / 1ps
+
+
+module RegisterID_EX(ReadData1In, ReadData1Out, ReadData2In, ReadData2Out,
+                     Instruction1In, Instruction1Out, Instruction2In, Instruction2Out, Instruction3In, Instruction3Out, 
