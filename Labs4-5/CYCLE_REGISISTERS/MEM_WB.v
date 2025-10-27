5832fcd9d5e95349ddfd2b03b6998b8d98d41e24
eharris2 <166444973+eharris2@users.noreply.github.com>
2025-10-27 14:27:29 -0700

    Update MEM_WB.v

--- Labs4-5/CYCLE_REGISISTERS/MEM_WB.v
+++ Labs4-5/CYCLE_REGISISTERS/MEM_WB.v
@@ -1,1 +1,19 @@
+`timescale 1ns / 1ps
 
+
+module MEM_WB(ReadDataIn, ReadDataOut, ALUResultIn, ALUResultOut, MemtoRegIn, MemtoRegOut, PCSrcIn, PCSrcOut, Clk);
+  input Clk;
+  input [31:0] ALUResultIn;
+  input [31:0] ReadDataIn;
+  input MemtoRegIn;
+  input PCSrcIn;
+  
+  always @(posedge Clk) begin
+    ALUResultOut <= ALUResultIn;
+    ReadDataOut <= ReadDataIn;
+    MemtoRegOut <= MemtoRegIn;
+    PCSrcOut <= PCSrcIn;
+  end
+
+endmodule
+  
