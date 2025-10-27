de99b3cfc677e2855510e92cd350e85654c5fdcc
nmonroe04 <167247190+nmonroe04@users.noreply.github.com>
2025-10-27 14:24:29 -0700

    Update IF_ID.v

--- Labs4-5/CYCLE_REGISISTERS/IF_ID.v
+++ Labs4-5/CYCLE_REGISISTERS/IF_ID.v
@@ -2,11 +2,12 @@
 
 
 
-module RegisterIF_ID(PCAddResult,instructionReadIn,instructionReadOut,Clk);
+module RegisterIF_ID(PCAddResult,instructionReadIn,PCAddResultOut,instructionReadOut,Clk);
 input Clk;
 input [31:0]  PCAddResult;
   input [31:0] instructionReadIn;
   output [31:0]instructionReadOut;
+  output [31:0] PCAddResultOut;
 
     always @(posedge Clk) begin
     
