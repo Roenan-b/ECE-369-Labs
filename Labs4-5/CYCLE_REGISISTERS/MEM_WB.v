963eeced6f795323a93566b98c1bf121323cd316
eharris2 <166444973+eharris2@users.noreply.github.com>
2025-10-27 14:52:37 -0700

    Update MEM_WB.v

--- Labs4-5/CYCLE_REGISISTERS/MEM_WB.v
+++ Labs4-5/CYCLE_REGISISTERS/MEM_WB.v
@@ -5,11 +5,15 @@ module MEM_WB(ReadDataIn, ReadDataOut, ALUResultIn, ALUResultOut, MemtoRegIn, Me
   input Clk;
   input [31:0] ALUResultIn;
   input [31:0] ReadDataIn;
+
+  //Controller Signals Inputs
   input MemtoRegIn;
   input RegWriteIn;
 
   output reg [31:0] ALUResultOut;
   output reg [31:0] ReadDataOut;
+
+  //Controller Signals Outputs
   output reg MemtoRegOut;
   output reg RegWriteOut;
   
