7bee7fe5ca2489dd92bf0c85f53fa401907e33d9
nmonroe04 <167247190+nmonroe04@users.noreply.github.com>
2025-10-29 14:28:01 -0700

    Update TOP_LEVEL_DESIGN_LAB_4.v

--- Labs4-5/TOP_LEVEL_DESIGN_LAB_4.v
+++ Labs4-5/TOP_LEVEL_DESIGN_LAB_4.v
@@ -14,14 +14,14 @@ module toplevel(instructionRead,CLk,instructionWrite);
   PCAdder a4(instructionRead, PCAddResult); //Takes instruction number and adds 4
 
   //FIRST STAGE REGISTER Fetch->Decode
-  IF_ID a13(PCAddResult,instruction,PCAddResultOut,instructionReadOut,Clk);
+  IF_ID a13(PCAddResult,instruction,PCAddResultOutofIF/ID,instructionReadOut,Clk);
 
   //SECOND STAGE REGISTER Decode->Execute
 RegisterID_EX a14(ALUSrcIn,ALUopIn,RegDstIn,ALUSrcOut,ALUopOut,RegDstOut,BranchIn,MemWriteIn,MemReadIn,
                      BranchOut,MemWriteOut,MemReadOut,MemToRegIn,RegWriteIn,MemToRegOut,RegWriteOut,
-                     ReadData1In,ReadData2In,PCAddResultIn,signResultIn,RTRegdestIn,RDRegdestIn,
-                     ReadData1Out,ReadData2Out,PCAddResultOut,signResultOut,RTRegdestIn,
-                     RDRegdestIn);
+                     ReadData1In,ReadData2In,PCAddResultOutofIF/ID,signResultIn,rt,rd,
+                     ReadData1Out,ReadData2OutofID/ED,PCAddResultOutofID/EX,signResultOut,RTRegdestOut,
+                     RDRegdestOut);
   //THIRD STAGE REGISTER Execute->Memory
   EX_MEM a15(AddResultIn, AddResultOut, ALUResultIn, ALUResultOut, MuxIn, MuxOut, ReadData2In, ReadData2Out, ZeroIn, ZeroOut,
               MemWriteIn, MemWriteOut, MemReadIn, MemReadOut, BranchIn, BranchOut, MemtoRegIn, MemtoRegOut, RegWriteIn, RegWriteOut, Clk);
@@ -29,19 +29,22 @@ RegisterID_EX a14(ALUSrcIn,ALUopIn,RegDstIn,ALUSrcOut,ALUopOut,RegDstOut,BranchI
   MEM_WB a16(ReadDataIn, ReadDataOut, ALUResultIn, ALUResultOut, MemtoRegIn, MemtoRegOut, RegWriteIn, RegWriteOut, Clk);
   
   
-  assign opCode = instructionReadOut [31:26]
-    assign   rs = instructionReadOut [25:21]
-    assign   rt = instructionReadOut [20:16]
+  assign opCode = instructionReadOut [31:26];
+    assign   rs = instructionReadOut [25:21];
+  assign   rt = instructionReadOut [20:16];
       assign rd = instructionReadOut [15:11];
   assign shamt = instructionReadOut [10:6]; 
   assign funct = instructionReadOut [5:0];
-
   
-  controller a2(instructionReadOut, Clk, ALUSrc, RegDst, OPCode, MemRead, MemWrite, MemtoReg, RegWrite, Branch,Jump); //Check but should be good
+  //For I-type instructions
+ assign imm = instructionRead [15:0];
 
-  SignExtension a3(imm, signResult);
+  //All "in"-suffix ports feed into ID/EX register
+  controller a2(instructionReadOut, Clk, ALUSrcIn, RegDstIn, OPCodeIn, MemReadIn, MemWriteIn, MemtoRegIn, RegWriteIn, BranchIn,Jump); //Check but should be good
 
-  RegisterFile a5(rs, rt, WriteRegister, WriteData, RegWrite, Clk, ReadData1, ReadData2); //Should be good
+  SignExtension a3(imm, signResultIn); //signResultIn goes to ID/EX reg
+
+  RegisterFile a5(rs, rt, WriteRegister, WriteData, RegWrite, Clk, ReadData1In, ReadData2In); //Should be good
 
   Mux32Bit2To1 a6(WriteRegister, rt, rd, RegDst);  //$rd vs imm mux, uses regDst as signal
 
