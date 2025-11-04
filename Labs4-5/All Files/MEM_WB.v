86ab15a01931794d75f8c26c844572ca4bf3be84
Roenan Bingle <134002753+Roenan-b@users.noreply.github.com>
2025-11-04 16:09:41 -0700

    Update MEM_WB.v

--- Labs4-5/All Files/MEM_WB.v
+++ Labs4-5/All Files/MEM_WB.v
@@ -1,1 +1,36 @@
+`timescale 1ns / 1ps
 
+
+module MEM_WB(ReadDataIn, ReadDataOut, ALUResultIn, ALUResultOut, MemtoRegIn, MemtoRegOut, RegWriteIn, RegWriteOut, Clk, Reset);
+  input Clk;
+  input Reset;
+  input [31:0] ALUResultIn;
+  input [31:0] ReadDataIn;
+
+  //Controller Signals Inputs
+  input MemtoRegIn;
+  input RegWriteIn;
+
+  output reg [31:0] ALUResultOut;
+  output reg [31:0] ReadDataOut;
+
+  //Controller Signals Outputs
+  output reg MemtoRegOut;
+  output reg RegWriteOut;
+  
+  always @(posedge Clk) begin
+    if(Reset) begin
+        ALUResultOut <= 32'b0;
+        ReadDataOut <= 32'b0;
+        MemtoRegOut <= 1'b0;
+        RegWriteOut <= 1'b0;
+     end else begin
+    ALUResultOut <= ALUResultIn;
+    ReadDataOut <= ReadDataIn;
+    MemtoRegOut <= MemtoRegIn;
+    RegWriteOut <= RegWriteIn;
+  end
+  end
+
+endmodule
+  
