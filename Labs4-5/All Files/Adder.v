5720d681d9275c03751dc9ae54089127dfa6c87a
Roenan Bingle <134002753+Roenan-b@users.noreply.github.com>
2025-11-04 16:07:33 -0700

    Update Adder.v

--- Labs4-5/All Files/Adder.v
+++ Labs4-5/All Files/Adder.v
@@ -1,1 +1,12 @@
+module Adder(PCAddResult, immSL2, InstructionSig);
 
+    input [31:0] immSL2;         
+    input [31:0] PCAddResult;
+    output reg [31:0] InstructionSig;
+
+    // Always block triggers whenever PCResult changes
+  always @ (*) begin
+        InstructionSig <= PCAddResult + immSL2; // Increment the PC by 4 (word-aligned instruction step)
+    end
+  
+endmodule
