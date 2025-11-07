4d493efec5529efef88ae2d7f6d3147eb33af063
Roenan Bingle <134002753+Roenan-b@users.noreply.github.com>
2025-11-06 19:03:59 -0700

    Update MEM_WB.v

--- Labs4-5/Lab5FInal/MEM_WB.v
+++ Labs4-5/Lab5FInal/MEM_WB.v
@@ -1,6 +1,10 @@
 `timescale 1ns / 1ps
 
 
+
+//Percent Effort
+// Roes: 33% Evan: 33% Noah: 33%
+
 module MEM_WB(ReadDataIn, ReadDataOut, ALUResultIn, ALUResultOut, MemtoRegIn, MemtoRegOut, RegWriteIn, RegWriteOut, Clk, Reset, WriteRegOut, WriteRegIn,PCResultIn,
 PCResultOut);
   input Clk;
