ac2b5cca8f9d0319ada589d3c8b07dbb9dadfc84
Roenan Bingle <134002753+Roenan-b@users.noreply.github.com>
2025-11-06 19:07:03 -0700

    Update Two4DigitDisplay.v

--- Labs4-5/Lab5FInal/Two4DigitDisplay.v
+++ Labs4-5/Lab5FInal/Two4DigitDisplay.v
@@ -1,4 +1,9 @@
 `timescale 1ns / 1ps
+
+
+
+//Percent Effort
+// Roes: 33% Evan: 33% Noah: 33%
 //////////////////////////////////////////////////////////////////////////////////
 // The University of Arizona
 // Electrical and Computer Engineering 
