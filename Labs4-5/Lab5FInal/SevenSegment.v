2a8b3f1374e8a3ac81bdf39ecf00247f91648af9
Roenan Bingle <134002753+Roenan-b@users.noreply.github.com>
2025-11-06 19:06:24 -0700

    Update SevenSegment.v

--- Labs4-5/Lab5FInal/SevenSegment.v
+++ Labs4-5/Lab5FInal/SevenSegment.v
@@ -1,4 +1,9 @@
 `timescale 1ns / 1ps
+
+
+
+//Percent Effort
+// Roes: 33% Evan: 33% Noah: 33%
 module SevenSegment(numin, segout);
     input	[3:0] numin;
     output	reg [6:0] segout;    //segout[6] - seg_a, segout[5] - seg_b, segout[4] - seg_c,
