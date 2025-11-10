36d8ced6ae18958aee76994050e41b76acd89f85
eharris2 <166444973+eharris2@users.noreply.github.com>
2025-11-09 19:34:38 -0700

    Update MEM_WB.v

--- Labs4-5/Lab4Final/MEM_WB.v
+++ Labs4-5/Lab4Final/MEM_WB.v
@@ -2,7 +2,7 @@
 
 
 module MEM_WB(ReadDataIn, ReadDataOut, ALUResultIn, ALUResultOut, MemtoRegIn, MemtoRegOut, RegWriteIn, RegWriteOut, Clk, Reset, WriteRegOut, WriteRegIn,PCResultIn,
-PCResultOut);
+PCResultOut, BranchTakenIn, BranchTakenOut);
   input Clk;
   input Reset;
   input [31:0] ALUResultIn;
@@ -12,6 +12,7 @@ PCResultOut);
   input [1:0] MemtoRegIn;
   input RegWriteIn;
   input[4:0] WriteRegIn;
+  input BranchTakenIn;
 
   output reg [31:0] ALUResultOut;
   output reg [31:0] ReadDataOut;
@@ -20,6 +21,9 @@ PCResultOut);
   output reg [1:0] MemtoRegOut;
   output reg RegWriteOut;
   output reg [4:0] WriteRegOut;
+  output BranchTakenOut;
+  
+  reg BranchTaken_q;
   
   always @(posedge Clk) begin
     if(Reset) begin
@@ -27,6 +31,7 @@ PCResultOut);
         ReadDataOut <= 32'b0;
         MemtoRegOut <= 2'b00;
         RegWriteOut <= 1'b0;
+        BranchTaken_q <= 1'b0;
      end else begin
     ALUResultOut <= ALUResultIn;
     ReadDataOut <= ReadDataIn;
@@ -34,8 +39,10 @@ PCResultOut);
     RegWriteOut <= RegWriteIn;
     WriteRegOut <= WriteRegIn;
     PCResultOut <= PCResultIn;
+    BranchTaken_q <= BranchTakenIn;
   end
   end
+  assign BranchTakenOut = BranchTaken_q;
 
 endmodule
   
